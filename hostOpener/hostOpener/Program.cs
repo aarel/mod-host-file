232e60c2c936ce087dbda093dd29393b4187e68b
Adam Arellano <7grxt1s@pm.me>
2025-09-03 02:53:57 -0500

    Added host file modifcation script

---
+++ hostOpener/hostOpener/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace hostOpener
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter host:");
+            string hosts = Console.ReadLine();
+            if (accessHosts.modifyHosts(hosts))
+            {
+                Console.WriteLine("hosts file modified.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ hostOpener/hostOpener/accessHosts.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace hostOpener
+{
+    public class accessHosts
+    {
+        public static bool modifyHosts(string host)
+        {
+            try
+            {
+                using (StreamWriter w = File.AppendText(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), @"drivers\etc\hosts")))
+                {
+                    w.WriteLine(host);
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+}
